9558984683455e379930ee10368bc718bb6c7f72
lightspot <zinnurshamsutdinov@yandex.ru>
2023-02-13 22:22:29 +0300

    Sprint 10 review 1

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("films")
+@RequestMapping("/films")
 @Validated
 public class FilmController {
 
--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmValidationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmValidationTests.java
@@ -29,15 +29,15 @@ public class FilmValidationTests {
 
     @Test
     public void shouldNotValidateIfNameIsNull() {
-        Film nullFilm = new Film(0, "", "description", LocalDate.of(2019, 8, 12), 120, Collections.singleton(0));
+        Film nullFilm = new Film(0, "", "description", LocalDate.of(2019, 8, 12), 120);
         assertThrows(ValidationException.class, () -> {
             filmController.addFilm(nullFilm);
         });
-        Film noFilm = new Film(1, "", "description", LocalDate.of(2019, 8, 12), 120, Collections.singleton(0));
+        Film noFilm = new Film(1, "", "description", LocalDate.of(2019, 8, 12), 120);
         assertThrows(ValidationException.class, () -> {
             filmController.addFilm(noFilm);
         });
-        Film gapFilm = new Film(2, "", "description", LocalDate.of(2019, 8, 12), 120, Collections.singleton(0));
+        Film gapFilm = new Film(2, "", "description", LocalDate.of(2019, 8, 12), 120);
         assertThrows(ValidationException.class, () -> {
             filmController.addFilm(gapFilm);
         });
@@ -52,7 +52,7 @@ public class FilmValidationTests {
                         + "возможность обрести мировую известность привлекают одаренных, но начинающих актеров с разных уголков страны, стремящихся отобрать "
                         + "популярность у «старой гвардии» Голливуда, которым предстоит бороться за личное будущее в динамично развивающейся киноиндустрии. "
                         + "Пока звезда немого кино Джек Конрад пытается найти себя в мире звуковых фильмов, дерзкая Нелли из Нью-Джерси, помощник продюсера "
-                        + "Мэнни и джазовый музыкант Сидни пробивают собственный путь к славе…", LocalDate.of(1922, 7, 21), 120, Collections.singleton(120));
+                        + "Мэнни и джазовый музыкант Сидни пробивают собственный путь к славе…", LocalDate.of(1922, 7, 21), 120);
         ValidationException thrown = assertThrows(ValidationException.class, () -> {
             filmController.addFilm(film);
         });
@@ -62,14 +62,14 @@ public class FilmValidationTests {
     @Test
     public void shouldNotValidateOldReleaseDate() {
         Film film = new Film(1, "name", "description",
-                LocalDate.of(1895, 3, 22), 1, Collections.singleton(120));
+                LocalDate.of(1895, 3, 22), 1);
         ValidationException thrown = assertThrows(ValidationException.class, () -> {
             filmController.addFilm(film);
         });
         assertEquals("Дата релиза фильма должна быть после 1895-12-28", thrown.getMessage());
 
         Film film2 = new Film(0, "Пограничный фильм", "description",
-                LocalDate.of(1895, 12, 28), 1, Collections.singleton(0));
+                LocalDate.of(1895, 12, 28), 1);
         assertDoesNotThrow(() -> {
             filmController.addFilm(film2);
         });
@@ -78,14 +78,14 @@ public class FilmValidationTests {
     @Test
     public void shouldNotValidateIfDurationIsNegative() {
         Film film = new Film(0, "Minus 1", "Movie back in time",
-                LocalDate.of(2000, 1, 1), -1, Collections.singleton(120));
+                LocalDate.of(2000, 1, 1), -7);
         ValidationException thrown = assertThrows(ValidationException.class, () -> {
             filmController.addFilm(film);
         });
         assertEquals("Продолжительность фильма не может быть отрицательным.", thrown.getMessage());
 
         Film film2 = new Film(1, "Фильм с нулевой длительностью", "но не в секундах",
-                LocalDate.of(2000, 1, 1), 0, Collections.singleton(0));
+                LocalDate.of(2000, 1, 1), 0);
         assertDoesNotThrow(() -> {
             filmController.addFilm(film2);
         });
